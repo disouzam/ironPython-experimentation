5454edf05674c20d3f76429bee3d09ba260d6435
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2023-12-22 18:24:26 -0300

    refactor: Adopted file-scoped namespace

--- IronPythonIntro/HelloWorld/Program.cs
+++ IronPythonIntro/HelloWorld/Program.cs
@@ -1,17 +1,16 @@
-﻿namespace HelloWorld
+﻿namespace HelloWorld;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var eng = IronPython.Hosting.Python.CreateEngine();
-            var scope = eng.CreateScope();
-            eng.Execute(
-            @"def greetings(name):
+        var eng = IronPython.Hosting.Python.CreateEngine();
+        var scope = eng.CreateScope();
+        eng.Execute(
+        @"def greetings(name):
                 return 'Hello ' + name.title() + '!'
             ", scope);
-            dynamic greetings = scope.GetVariable("greetings");
-            System.Console.WriteLine(greetings("world of Iron Python"));
-        }
+        dynamic greetings = scope.GetVariable("greetings");
+        System.Console.WriteLine(greetings("world of Iron Python"));
     }
 }
