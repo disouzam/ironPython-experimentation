e7da04bb276961ceacbb7b101db58421be166fbe
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2023-12-22 18:46:45 -0300

    feat: Replaced in-place script with script content being read from file

--- IronPythonIntro/HelloWorld/Program.cs
+++ IronPythonIntro/HelloWorld/Program.cs
@@ -1,15 +1,19 @@
-﻿namespace HelloWorld;
+﻿using System.Reflection;
+
+namespace HelloWorld;
 
 internal class Program
 {
     static void Main(string[] args)
     {
+        var basePath = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory);
+        var scriptFilePath = Path.Combine(basePath.FullName, "scriptFile.py");
+
+        var scriptContent = File.ReadAllText(scriptFilePath);
+
         var eng = IronPython.Hosting.Python.CreateEngine();
         var scope = eng.CreateScope();
-        eng.Execute(
-        @"def greetings(name):
-                return 'Hello ' + name.title() + '!'
-            ", scope);
+        eng.Execute(scriptContent, scope);
         dynamic greetings = scope.GetVariable("greetings");
         System.Console.WriteLine(greetings("world of Iron Python"));
     }
