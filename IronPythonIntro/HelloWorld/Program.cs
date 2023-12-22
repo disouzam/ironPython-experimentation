1ea03ade36ccfa447cc5968a285d7c83b78916c1
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2023-12-21 22:11:03 -0300

    feat: Added package IronPython version 3.4.0 to console project
    - And added first working code inside Main method
    - Code snippet was obtained from https://github.com/IronLanguages/ironpython3/tree/v3.4.1#examples

--- IronPythonIntro/HelloWorld/Program.cs
+++ IronPythonIntro/HelloWorld/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var eng = IronPython.Hosting.Python.CreateEngine();
+            var scope = eng.CreateScope();
+            eng.Execute(
+            @"def greetings(name):
+                return 'Hello ' + name.title() + '!'
+            ", scope);
+            dynamic greetings = scope.GetVariable("greetings");
+            System.Console.WriteLine(greetings("world of Iron Python"));
         }
     }
 }
