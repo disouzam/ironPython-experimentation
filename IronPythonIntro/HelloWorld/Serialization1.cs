dbf40c4f79aa84399c4ff0e55325b93f3db6d9a5
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2023-12-23 12:39:42 -0300

    feat: New class to call serialization1.py script using IronPython

---
+++ IronPythonIntro/HelloWorld/Serialization1.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld;
+
+internal class Serialization1
+{
+    public static void Run()
+    {
+        var basePath = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory);
+        var scriptFilePath = Path.Combine(basePath.FullName, "PythonScripts", "serialization1.py");
+
+        var scriptContent = File.ReadAllText(scriptFilePath, System.Text.Encoding.UTF8);
+
+        var eng = IronPython.Hosting.Python.CreateEngine();
+        var scope = eng.CreateScope();
+        eng.Execute(scriptContent, scope);
+    }
+}
